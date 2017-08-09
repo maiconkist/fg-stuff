2fbd499f7144dff3fec17f6d1f2df5a6fc19d2f9
Maicon Kist <maiconkist@gmail.com>
2017-08-09 14:24:05 +0000

    update

--- rx_single.py
+++ rx_single.py
@@ -4,7 +4,7 @@
 # GNU Radio Python Flow Graph
 # Title: OFDM Single
 # Description: Single
-# Generated: Tue Aug  8 15:42:21 2017
+# Generated: Wed Aug  9 13:19:04 2017
 ##################################################
 
 from gnuradio import blocks
@@ -36,16 +36,16 @@ class rx_single(gr.top_block):
         self.length_tag_key = length_tag_key = "frame_len"
         self.header_mod = header_mod = digital.constellation_bpsk()
         self.fft_len = fft_len = 64
-        self._usrpemuport_config = ConfigParser.ConfigParser()
-        self._usrpemuport_config.read('default')
-        try: usrpemuport = self._usrpemuport_config.get("usrpemu", "usrpemuport")
-        except: usrpemuport = "2666"
-        self.usrpemuport = usrpemuport
-        self._usrpemuip_config = ConfigParser.ConfigParser()
-        self._usrpemuip_config.read('default')
-        try: usrpemuip = self._usrpemuip_config.get("usrpemu", "usrpemuip")
-        except: usrpemuip = "127.0.0.1"
-        self.usrpemuip = usrpemuip
+        self._usrpport_config = ConfigParser.ConfigParser()
+        self._usrpport_config.read('default')
+        try: usrpport = self._usrpport_config.get("usrp", "port")
+        except: usrpport = "2666"
+        self.usrpport = usrpport
+        self._usrpip_config = ConfigParser.ConfigParser()
+        self._usrpip_config.read('default')
+        try: usrpip = self._usrpip_config.get("usrp", "ip")
+        except: usrpip = "127.0.0.1"
+        self.usrpip = usrpip
         self.sync_word2 = sync_word2 = [0j, 0j, 0j, 0j, 0j, 0j, (-1+0j), (-1+0j), (-1+0j), (-1+0j), (1+0j), (1+0j), (-1+0j), (-1+0j), (-1+0j), (1+0j), (-1+0j), (1+0j), (1+0j), (1 +0j), (1+0j), (1+0j), (-1+0j), (-1+0j), (-1+0j), (-1+0j), (-1+0j), (1+0j), (-1+0j), (-1+0j), (1+0j), (-1+0j), 0j, (1+0j), (-1+0j), (1+0j), (1+0j), (1+0j), (-1+0j), (1+0j), (1+0j), (1+0j), (-1+0j), (1+0j), (1+0j), (1+0j), (1+0j), (-1+0j), (1+0j), (-1+0j), (-1+0j), (-1+0j), (1+0j), (-1+0j), (1+0j), (-1+0j), (-1+0j), (-1+0j), (-1+0j), 0j, 0j, 0j, 0j, 0j]
         self.sync_word1 = sync_word1 = [0., 0., 0., 0., 0., 0., 0., 1.41421356, 0., -1.41421356, 0., 1.41421356, 0., -1.41421356, 0., -1.41421356, 0., -1.41421356, 0., 1.41421356, 0., -1.41421356, 0., 1.41421356, 0., -1.41421356, 0., -1.41421356, 0., -1.41421356, 0., -1.41421356, 0., 1.41421356, 0., -1.41421356, 0., 1.41421356, 0., 1.41421356, 0., 1.41421356, 0., -1.41421356, 0., 1.41421356, 0., 1.41421356, 0., 1.41421356, 0., -1.41421356, 0., 1.41421356, 0., 1.41421356, 0., 1.41421356, 0., 0., 0., 0., 0., 0.]
         self._samprate_config = ConfigParser.ConfigParser()
@@ -55,6 +55,16 @@ class rx_single(gr.top_block):
         self.samprate = samprate
         self.payload_equalizer = payload_equalizer = digital.ofdm_equalizer_simpledfe(fft_len, payload_mod.base(), occupied_carriers, pilot_carriers, pilot_symbols, 1)
         self.packet_len = packet_len = 96
+        self._myport_config = ConfigParser.ConfigParser()
+        self._myport_config.read('default')
+        try: myport = self._myport_config.get("rx", "port")
+        except: myport = "2666"
+        self.myport = myport
+        self._myip_config = ConfigParser.ConfigParser()
+        self._myip_config.read('default')
+        try: myip = self._myip_config.get("rx", "ip")
+        except: myip = "127.0.0.1"
+        self.myip = myip
         self.header_formatter = header_formatter = digital.packet_header_ofdm(occupied_carriers, n_syms=1, len_tag_key=packet_length_tag_key, frame_len_tag_key=length_tag_key, bits_per_header_sym=header_mod.bits_per_symbol(), bits_per_payload_sym=payload_mod.bits_per_symbol(), scramble_header=False)
         self.header_equalizer = header_equalizer = digital.ofdm_equalizer_simpledfe(fft_len, header_mod.base(), occupied_carriers, pilot_carriers, pilot_symbols)
         self._gain_config = ConfigParser.ConfigParser()
@@ -71,7 +81,22 @@ class rx_single(gr.top_block):
         ##################################################
         # Blocks
         ##################################################
-        self.zeromq_pull_source_0 = zeromq.pull_source(gr.sizeof_gr_complex, 1, "tcp://" + usrpemuip + ":" + usrpemuport, 100, True, -1)
+        self.zeromq_push_sink_0 = zeromq.push_sink(gr.sizeof_gr_complex, 1, "tcp://" + myip + ":" + myport, 100, True, -1)
+        self.zeromq_pull_source_0 = zeromq.pull_source(gr.sizeof_gr_complex, 1, "tcp://" + usrpip + ":" + usrpport, 100, True, -1)
+        self.digital_ofdm_tx_0 = digital.ofdm_tx(
+        	  fft_len=fft_len, cp_len=fft_len/4,
+        	  packet_length_tag_key=packet_length_tag_key,
+        	  occupied_carriers=occupied_carriers,
+        	  pilot_carriers=pilot_carriers,
+        	  pilot_symbols=pilot_symbols,
+        	  sync_word1=sync_word1,
+        	  sync_word2=sync_word2,
+        	  bps_header=1,
+        	  bps_payload=2,
+        	  rolloff=0,
+        	  debug_log=True,
+        	  scramble_bits=False
+        	 )
         self.digital_ofdm_rx_0 = digital.ofdm_rx(
         	  fft_len=fft_len, cp_len=fft_len/4,
         	  frame_length_tag_key='frame_'+'packet_len',
@@ -86,6 +111,14 @@ class rx_single(gr.top_block):
         	  debug_log=False,
         	  scramble_bits=False
         	 )
+        self.channels_channel_model_0_0 = channels.channel_model(
+        	noise_voltage=0.0,
+        	frequency_offset=0.0,
+        	epsilon=1.0,
+        	taps=(1.0 + 1.0j, ),
+        	noise_seed=0,
+        	block_tags=True
+        )
         self.channels_channel_model_0 = channels.channel_model(
         	noise_voltage=0.0,
         	frequency_offset=0.0,
@@ -94,17 +127,22 @@ class rx_single(gr.top_block):
         	noise_seed=0,
         	block_tags=True
         )
-        self.blocks_tuntap_pdu_0 = blocks.tuntap_pdu('tap0', 10000, True)
+        self.blocks_tuntap_pdu_0 = blocks.tuntap_pdu('tap0', 10000, False)
         self.blocks_tagged_stream_to_pdu_0 = blocks.tagged_stream_to_pdu(blocks.byte_t, 'packet_len')
         self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_char*1, "Rx'd Packets", ""); self.blocks_tag_debug_0.set_display(True)
+        self.blocks_pdu_to_tagged_stream_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, 'packet_len')
 
         ##################################################
         # Connections
         ##################################################
         self.msg_connect((self.blocks_tagged_stream_to_pdu_0, 'pdus'), (self.blocks_tuntap_pdu_0, 'pdus'))
+        self.msg_connect((self.blocks_tuntap_pdu_0, 'pdus'), (self.blocks_pdu_to_tagged_stream_0, 'pdus'))
+        self.connect((self.blocks_pdu_to_tagged_stream_0, 0), (self.digital_ofdm_tx_0, 0))
         self.connect((self.channels_channel_model_0, 0), (self.digital_ofdm_rx_0, 0))
+        self.connect((self.channels_channel_model_0_0, 0), (self.zeromq_push_sink_0, 0))
         self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_tag_debug_0, 0))
         self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_tagged_stream_to_pdu_0, 0))
+        self.connect((self.digital_ofdm_tx_0, 0), (self.channels_channel_model_0_0, 0))
         self.connect((self.zeromq_pull_source_0, 0), (self.channels_channel_model_0, 0))
 
     def get_pilot_symbols(self):
@@ -166,17 +204,17 @@ class rx_single(gr.top_block):
         self.set_payload_equalizer(digital.ofdm_equalizer_simpledfe(self.fft_len, payload_mod.base(), self.occupied_carriers, self.pilot_carriers, self.pilot_symbols, 1))
         self.set_header_equalizer(digital.ofdm_equalizer_simpledfe(self.fft_len, header_mod.base(), self.occupied_carriers, self.pilot_carriers, self.pilot_symbols))
 
-    def get_usrpemuport(self):
-        return self.usrpemuport
+    def get_usrpport(self):
+        return self.usrpport
 
-    def set_usrpemuport(self, usrpemuport):
-        self.usrpemuport = usrpemuport
+    def set_usrpport(self, usrpport):
+        self.usrpport = usrpport
 
-    def get_usrpemuip(self):
-        return self.usrpemuip
+    def get_usrpip(self):
+        return self.usrpip
 
-    def set_usrpemuip(self, usrpemuip):
-        self.usrpemuip = usrpemuip
+    def set_usrpip(self, usrpip):
+        self.usrpip = usrpip
 
     def get_sync_word2(self):
         return self.sync_word2
@@ -207,6 +245,18 @@ class rx_single(gr.top_block):
 
     def set_packet_len(self, packet_len):
         self.packet_len = packet_len
+
+    def get_myport(self):
+        return self.myport
+
+    def set_myport(self, myport):
+        self.myport = myport
+
+    def get_myip(self):
+        return self.myip
+
+    def set_myip(self, myip):
+        self.myip = myip
 
     def get_header_formatter(self):
         return self.header_formatter
