c666084c62ca906768861bc1edc5399c7bd268c8
Maicon Kist <maiconkist@gmail.com>
2017-08-09 08:53:17 +0000

    added simple tun test

---
+++ supertesttx.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Supertesttx
+# Generated: Wed Aug  9 08:21:41 2017
+##################################################
+
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import zeromq
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import ConfigParser
+
+
+class supertesttx(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Supertesttx")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+        self._payloadport_config = ConfigParser.ConfigParser()
+        self._payloadport_config.read('default')
+        try: payloadport = self._payloadport_config.get("split1", "payloadport")
+        except: payloadport = "2101"
+        self.payloadport = payloadport
+        self._ip_config = ConfigParser.ConfigParser()
+        self._ip_config.read('default')
+        try: ip = self._ip_config.get("split1", "ip")
+        except: ip = "127.0.0.1"
+        self.ip = ip
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.zeromq_push_sink_0 = zeromq.push_sink(gr.sizeof_char, 1, "tcp://" + ip + ":" + payloadport, 100, True, -1)
+        self.blocks_tuntap_pdu_0 = blocks.tuntap_pdu('tap0', 10000, True)
+        self.blocks_pdu_to_tagged_stream_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, 'packet_len')
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.blocks_tuntap_pdu_0, 'pdus'), (self.blocks_pdu_to_tagged_stream_0, 'pdus'))
+        self.connect((self.blocks_pdu_to_tagged_stream_0, 0), (self.zeromq_push_sink_0, 0))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+    def get_payloadport(self):
+        return self.payloadport
+
+    def set_payloadport(self, payloadport):
+        self.payloadport = payloadport
+
+    def get_ip(self):
+        return self.ip
+
+    def set_ip(self, ip):
+        self.ip = ip
+
+
+def main(top_block_cls=supertesttx, options=None):
+
+    tb = top_block_cls()
+    tb.start()
+    try:
+        raw_input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
