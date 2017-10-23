e70ba959661c5e3ce60e86d729b25601c162c5f6
Maicon Kist <maiconkist@gmail.com>
2017-10-23 10:18:02 +0100

    update

--- cp_to_containers.sh
+++ cp_to_containers.sh
@@ -1,9 +1,13 @@
-for i in 101 102 103 104; do
+#!/bin/bash
+
+curdir=`dirname ${BASH_SOURCE[*]}`
+
+for i in 101 102 104; do
 	echo "Copying file to 192.168.10.$i"
-	scp *.py *.grc default root@192.168.10.$i:~/fg-stuff/ > /dev/null
+	scp ${curdir}/*.py ${curdir}/*.grc ${curdir}/default root@192.168.10.$i:~/fg-stuff/ > /dev/null
 done
 
 for i in 20 30; do
 	echo "Copying file to 192.168.10.$i"
-	scp *.py *.grc default connect@192.168.10.$i:~/fg-stuff/ > /dev/null
+	scp ${curdir}/*.py ${curdir}/*.grc ${curdir}/default connect@192.168.10.$i:~/fg-stuff/ > /dev/null
 done
