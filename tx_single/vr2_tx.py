4b7145a60d3ff41374a582ace900eae49ee609ff
Maicon Kist <maiconkist@gmail.com>
2017-11-29 14:40:57 +0000

    Using HyDRA Source.

--- tx_single/vr2_tx.py
+++ tx_single/vr2_tx.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: VR2 TX all-in-one
-# Generated: Fri Nov  3 11:03:15 2017
+# Generated: Wed Nov 29 12:21:33 2017
 ##################################################
 
 from gnuradio import blocks
@@ -11,7 +11,6 @@ from gnuradio import digital
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio import zeromq
-from gnuradio.digital.utils import tagged_streams
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
@@ -31,6 +30,11 @@ class vr2_tx(gr.top_block):
         self.pattern2 = pattern2 = [1, -1, 1, -1]
         self.pattern1 = pattern1 = [0., 1.41421356, 0., -1.41421356]
         self.fft_len = fft_len = 64
+        self._usrpip_config = ConfigParser.ConfigParser()
+        self._usrpip_config.read('./default')
+        try: usrpip = self._usrpip_config.get("usrp_hydra", "ip")
+        except: usrpip = "127.0.0.1"
+        self.usrpip = usrpip
         self._txoutport_config = ConfigParser.ConfigParser()
         self._txoutport_config.read('./default')
         try: txoutport = self._txoutport_config.get("vr2_tx", "port")
@@ -43,10 +47,14 @@ class vr2_tx(gr.top_block):
         try: samprate = self._samprate_config.getfloat("usrp_hydra", "samprate2")
         except: samprate = 0
         self.samprate = samprate
+        self._rxport_config = ConfigParser.ConfigParser()
+        self._rxport_config.read('./default')
+        try: rxport = self._rxport_config.get("usrp_hydra", "rxport2")
+        except: rxport = "2666"
+        self.rxport = rxport
         self.pilot_symbols = pilot_symbols = ((1, 1, -1, -1),)
         self.packet_len = packet_len = 100
         self.occupied_carriers = occupied_carriers = (sorted(tuple(set([x for x in range(-26,27)]) - set(pilot_carriers[0]) - set([0,]))),)
-        self.length_tag_key = length_tag_key = "packet_len"
         self._ip_config = ConfigParser.ConfigParser()
         self._ip_config.read('./default')
         try: ip = self._ip_config.get("vr2_tx", "ip")
@@ -57,9 +65,10 @@ class vr2_tx(gr.top_block):
         # Blocks
         ##################################################
         self.zeromq_push_sink_0 = zeromq.push_sink(gr.sizeof_gr_complex, 1, "tcp://" + ip + ":" + txoutport, 100, True, -1)
+        self.zeromq_pull_source_0 = zeromq.pull_source(gr.sizeof_gr_complex, 1, "tcp://" + usrpip + ":" + rxport, 100, True, -1)
         self.digital_ofdm_tx_0 = digital.ofdm_tx(
         	  fft_len=fft_len, cp_len=16,
-        	  packet_length_tag_key=length_tag_key,
+        	  packet_length_tag_key="packet_len",
         	  occupied_carriers=occupied_carriers,
         	  pilot_carriers=pilot_carriers,
         	  pilot_symbols=pilot_symbols,
@@ -71,8 +80,23 @@ class vr2_tx(gr.top_block):
         	  debug_log=False,
         	  scramble_bits=False
         	 )
+        self.digital_ofdm_rx_0 = digital.ofdm_rx(
+        	  fft_len=64, cp_len=16,
+        	  frame_length_tag_key='frame_'+"packet_len",
+        	  packet_length_tag_key="packet_len",
+        	  occupied_carriers=occupied_carriers,
+        	  pilot_carriers=pilot_carriers,
+        	  pilot_symbols=pilot_symbols,
+        	  sync_word1=sync_word1,
+        	  sync_word2=sync_word2,
+        	  bps_header=1,
+        	  bps_payload=1,
+        	  debug_log=False,
+        	  scramble_bits=False
+        	 )
         self.blocks_throttle_0_0 = blocks.throttle(gr.sizeof_gr_complex*1, 500e3,True)
-        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, packet_len, length_tag_key)
+        self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_char*1, '', ""); self.blocks_tag_debug_0.set_display(True)
+        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, packet_len, "packet_len")
         self.analog_random_source_x_0 = blocks.vector_source_b(map(int, numpy.random.randint(5, 10, 1000)), True)
 
         ##################################################
@@ -81,7 +105,9 @@ class vr2_tx(gr.top_block):
         self.connect((self.analog_random_source_x_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))    
         self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.digital_ofdm_tx_0, 0))    
         self.connect((self.blocks_throttle_0_0, 0), (self.zeromq_push_sink_0, 0))    
+        self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_tag_debug_0, 0))    
         self.connect((self.digital_ofdm_tx_0, 0), (self.blocks_throttle_0_0, 0))    
+        self.connect((self.zeromq_pull_source_0, 0), (self.digital_ofdm_rx_0, 0))    
 
     def get_pilot_carriers(self):
         return self.pilot_carriers
@@ -112,6 +138,12 @@ class vr2_tx(gr.top_block):
         self.set_sync_word2([0., 0., 0., 0., 0., 0.,] + self.pattern2 * ((self.fft_len-12)/len(self.pattern2))  +[0., 0., 0., 0., 0., 0.,] )
         self.set_sync_word1([0., 0., 0., 0., 0., 0.,] + self.pattern1 * ((self.fft_len-12)/len(self.pattern1))  +[0., 0., 0., 0., 0., 0.,] )
 
+    def get_usrpip(self):
+        return self.usrpip
+
+    def set_usrpip(self, usrpip):
+        self.usrpip = usrpip
+
     def get_txoutport(self):
         return self.txoutport
 
@@ -136,6 +168,12 @@ class vr2_tx(gr.top_block):
     def set_samprate(self, samprate):
         self.samprate = samprate
 
+    def get_rxport(self):
+        return self.rxport
+
+    def set_rxport(self, rxport):
+        self.rxport = rxport
+
     def get_pilot_symbols(self):
         return self.pilot_symbols
 
@@ -155,12 +193,6 @@ class vr2_tx(gr.top_block):
 
     def set_occupied_carriers(self, occupied_carriers):
         self.occupied_carriers = occupied_carriers
-
-    def get_length_tag_key(self):
-        return self.length_tag_key
-
-    def set_length_tag_key(self, length_tag_key):
-        self.length_tag_key = length_tag_key
 
     def get_ip(self):
         return self.ip
